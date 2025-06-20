cefa24d2cc2e247543505d77ace976e486fc5e17
Yuvraj808 <yuvrajg151@gmail.com>
2025-06-20 23:55:07 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# My Portfolio
+
+This is a personal portfolio website built using [React](https://reactjs.org/), [Vite](https://vitejs.dev/), and [Tailwind CSS](https://tailwindcss.com/).
+
+## 🚀 Demo
+
+[Live Preview](http://localhost:5173/).
+
+## 🛠️ Tech Stack
+
+- **Frontend**: React, Vite
+- **Styling**: Tailwind CSS v4
+
+## 📂 Project Structure
+
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="flex justify-center items-center w-screen h-screen bg-zinc-900 text-white">
+      <h1 className="text-5xl font-bold text-center">Tailwind v4 is working 🎉</h1>
+    </div>
+  );
+}
+
